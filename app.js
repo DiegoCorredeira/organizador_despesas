6b10a4b0044d4e71970e9a195af562f8ed874f38
Diego Corredeira <91692393+DiegoCorredeira@users.noreply.github.com>
2023-05-19 18:09:04 -0300

    add function atualizarTabela, removerDespesa, atualizarPoupanca

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let poupanca = 0.0
 function adicionarDespesa(){
     let despesaInput = document.getElementById('nomeDespesa')
     let valorDespesaInput = document.getElementById('valorDespesa')
-    let nomeDespesa = valorDespesaInput.value.trim()
+    let nomeDespesa = despesaInput.value.trim()
     let valorDespesa = parseFloat(valorDespesaInput.value)
 
 
@@ -21,13 +21,15 @@ function adicionarDespesa(){
     despesaInput.value = ""
     valorDespesaInput.value = ""
     atualizarValorTotal()
+    atualizarTabelaDespesas()
+    atualizarPoupanca()
 
 }
 
 
 
 function atualizarValorTotal(){
-    let totalDespesas = despesas.reduce((total, despesas) => total + despesas.value, 0)
+    let totalDespesas = despesas.reduce((total, despesas) => total + despesas.valor, 0)
     document.getElementById("totalDespesas").textContent = "Total de despesas: R$" + totalDespesas.toFixed(2)
 
     let necessidades = salario * 0.5
@@ -48,4 +50,52 @@ function definirSalario(){
     salario = valorSalario
     salarioInput.value = ""
     atualizarValorTotal()
+    atualizarPoupanca() 
+}
+
+
+
+function atualizarTabelaDespesas(){
+    let tabelaDespesas = document.getElementById("corpoTabelaDespesas")
+    tabelaDespesas.innerHTML = ""
+
+    despesas.forEach((despesa, index) => {
+        let linha = document.createElement("tr")
+
+        let colunaNome = document.createElement("td")
+        colunaNome.textContent = despesa.nome
+
+        let colunaValor = document.createElement("td")
+        colunaValor.textContent = "R$" + despesa.valor.toFixed(2)
+
+        let colunaAcoes = document.createElement("td")
+
+        let btnRemover =  document.createElement("button")
+        btnRemover.textContent = "Remover"
+        btnRemover.classList.add("btn-remover")
+        btnRemover.onclick = function(){
+            removerDespesa(index)
+        }
+        colunaAcoes.appendChild(btnRemover)
+        linha.appendChild(colunaNome)
+        linha.appendChild(colunaValor)
+        linha.appendChild(colunaAcoes)
+
+        tabelaDespesas.appendChild(linha)
+
+    })
+
+}
+
+function removerDespesa(index) {
+    despesas.splice(index, 1)
+    atualizarValorTotal()
+    atualizarTabelaDespesas()
+    atualizarPoupanca()
+}
+
+function atualizarPoupanca(){
+    let totalDespesas = despesas.reduce((total, despesas) => total + despesas.valor, 0)
+    poupanca = salario * 0.2 - totalDespesas
+    document.getElementById("poupanca").textContent = "Poupança (20%)" + poupanca.toFixed(2)
 }
