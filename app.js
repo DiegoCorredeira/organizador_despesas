55911a8d2372d5471dfe997b7cd23d823c91500e
Diego Corredeira <91692393+DiegoCorredeira@users.noreply.github.com>
2023-05-19 17:33:36 -0300

    add function "adicionar despesas"

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ let despesas = []
 let salario = 0.0
 let poupanca = 0.0
 
+function adicionarDespesa(){
+    let despesaInput = document.getElementById('nomeDespesa')
+    let valorDespesaInput = document.getElementById('valorDespesa')
+    let nomeDespesa = valorDespesaInput.value.trim()
+    let valorDespesa = parseFloat(valorDespesaInput.value)
+
+
+    if (nomeDespesa === "" || isNaN(valorDespesa) || valorDespesa <= 0) {
+        alert('Informe um nome e um valor válidos para a despesa')
+        return
+    }
+    despesas.push({
+        nome: nomeDespesa,
+        valor: valorDespesa
+    })
+    
+    despesaInput.value = ""
+    valorDespesaInput.value = ""
+
+}
+
+
 function atualizarTabelaDespesas(){
     let totalDespesas = despesas.reduce((total, despesas => total + despesas.value, 0))
     document.getElementById("totalDespesas").textContent = "Total de despesas: R$" + totalDespesas.toFixed(2)
